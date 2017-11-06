941e7a0d25bfcf998b18d32ade30810348c68b8d
Neena van de Water <nkuriakose@hotmail.com>
2017-11-06 17:11:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,13 +11,12 @@ end
 
 def batch_badge_creator(array)
   array.collect { | name |
-  return name }
+  return "Hello, my name is #{array}." }
 end
 
 
-def assign_rooms(attendees)
+def assign_rooms(attendee)
   attendees.map { |e|
-   return e
+   return "Hello, #{attendee}! You'll be assigned to room #{number}!"
    }
-
 end
