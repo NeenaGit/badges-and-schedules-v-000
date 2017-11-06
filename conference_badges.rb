82000db1a8211fece884e5437d31ff3d16073f1f
Neena van de Water <nkuriakose@hotmail.com>
2017-11-06 17:16:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,6 @@ end
 
 def assign_rooms(attendee)
    attendees.each_with_index do |name, index|
-   return "Hello, #{name}! You'll be assigned to room #{index +1}!"
+   return "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
 end
