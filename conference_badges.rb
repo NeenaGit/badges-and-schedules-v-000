94fb2b1de24cfa4acdaef74bd7d323327071d60c
Neena van de Water <nkuriakose@hotmail.com>
2017-11-06 16:57:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  array.collect { | names |
-  return "Hello, my name is #{array}." }
+  array.collect { | name |
+  return "Hello, my name is #{name}." }
 
 end
